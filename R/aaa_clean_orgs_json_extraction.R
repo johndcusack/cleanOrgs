4c4848a19c17302085d48b12abd5ca9c49a6f9e1
johndcusack <129303439+johndcusack@users.noreply.github.com>
2025-06-22 12:30:17 +0100

    cleanup stage 1
    revised names, edited roxygen comments, started removing namespace checks

---
+++ R/aaa_clean_orgs_json_extraction.R
@@ -0,0 +1,52 @@
+
+clean_orgs_json_extraction <- function(df,code_column) {
+  #' Extract ODS json list
+  #'
+  #' @description
+    #' This function takes a column of NHS organisation codes and returns a list of
+    #' JSON objects extracted from the Organisational Data Service's API.
+    #' This is an internal helper function that gets used by clean_orgs_get_json.R, which
+    #' wraps this function with memoise to cache results.
+    #' Caching prevents multiple repeated calls to the API,
+    #' reduces demand on the API itself and
+    #' speeds up the function when used repeatedly.
+  #'
+  #' @param df the dataframe that has the column containing organisation codes
+  #' @param code_column the column within the dataframe that has the organisation codes
+  #'
+  #' @keywords internal
+  #' @import httr2
+
+
+
+
+  if (!code_column %in% names(df)) {
+    stop("The specified code_column does not exist in the dataframe.")
+  }
+
+  codes <- unique(na.omit(df[[code_column]]))
+
+# list assignment in R is more efficient in lists with a pre-allocated length so
+# that's what I'm doing here
+  json_list <- vector("list",length = length(codes))
+  base_url <- "https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations/"
+
+  for (i in seq_along(codes)) {
+    code <- codes[i]
+    full_url <-  paste0(base_url,code)
+
+    tryCatch({
+    response <-  httr2::request(full_url) |>
+      httr2::req_headers(Accept = 'application/json') |>
+      httr2::req_perform()
+
+    json_data <- httr2::resp_body_json(response)
+
+    json_list[[i]] <- json_data
+    },error = function(e) {
+      warning(sprintf("Failed to retrieve data for code %s: %s", code, e$message))
+      json_list[[i]] <- NULL
+    })
+  }
+  return(json_list)
+}
---
+++ R/clean_orgs_update_names.R
@@ -0,0 +1,47 @@
+clean_orgs_update_names <- function(df,code_column,name_column,verbose=TRUE) {
+
+  #' Update organisation names
+  #'
+  #' This function adds or replaces a column in a dataframe with the current
+  #' organisation name as recorded by the Organisation Data Service (ODS).
+  #' It uses an organisation code column in the dataframe, retrieves JSON data
+  #' for each code using the ODS API (`cached_ods_json_list()`), and constructs
+  #' a lookup table via `create_ods_table()`.
+  #'
+  #' Note: This function does not merge or update superseded trusts and will use
+  #' their most recent recorded name. To account for mergers, use
+  #' `replace_with_successor()` before calling this function.
+  #'
+  #' @param df A dataframe containing organisation codes.
+  #' @param code_column A string. The name of the column in `df` that contains organisation codes.
+  #' @param name_column A string. The name of the column to be added/overwritten with organisation names.
+  #' @param verbose Logical. If TRUE, prints the number of unrecognised codes.
+  #'
+  #' @return A dataframe identical to `df`, with an additional or updated `name_column`.
+  #'
+  #' @importFrom dplyr coalesce
+  #'
+  #' @export
+
+
+  if (!code_column %in% names(df)) {
+    stop("The specified code_column does not exist in the dataframe.")
+  }
+
+  org_list <- clean_orgs_get_json(df,code_column)
+  ods_table <-  clean_orgs_create_ods_table(org_list) |> dplyr::select(-successor_code)
+
+
+  org_lookup <- setNames(ods_table$org_name, ods_table$org_code)
+
+  df[[name_column]] <-dplyr::coalesce(
+    unname(org_lookup[as.character(df[[code_column]])]),
+    "Unknown"
+  )
+
+  if (verbose){
+    unknowns <- sum(df[[name_column]] == "Unknown", na.rm = TRUE)
+    message(unknowns, " unrecognised organisation codes, ",name_column," set to 'Unknown'.")
+  }
+  return(df)
+}
