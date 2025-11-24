db8cfc0ca21bb8dd80054121705d7a6ed0e2a568
johndcusack <john.cusack@nhs.net>
2025-11-24 09:48:07 +0000

    testing replace_succeeded

---
+++ tests/testthat/test-cleanOrgs_replace_succeeded.R
@@ -0,0 +1,15 @@
+test_that("cleanOrgs_replace_succeeded replaces superseded codes", {
+  df <- data.frame(org_code = c("R1C"), stringsAsFactors = FALSE)
+
+  out <- cleanOrgs::cleanOrgs_replace_succeeded(df, code_column = "org_code")
+
+  expect_equal(out$org_code[df$org_code == "R1C"], "RW1")
+})
+
+test_that("cleanOrgs_replace_succeeded leaves non-superseded codes unchanged", {
+  df <- data.frame(org_code = c("RTH"), stringsAsFactors = FALSE)
+
+  out <- cleanOrgs::cleanOrgs_replace_succeeded(df, code_column = "org_code")
+
+  expect_equal(out$org_code[df$org_code == "RTH"], "RTH")
+})
